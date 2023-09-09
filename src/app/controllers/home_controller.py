91eb95d71296d1b40362b5ed72f3a03264cdd8d3
davidsantana06 <davidsantana06@hotmail.com>
2023-09-09 20:49:53 -0300

    feat(error): improve error page layout

--- src/app/controllers/home_controller.py
+++ src/app/controllers/home_controller.py
@@ -2,7 +2,7 @@ from flask import render_template
 
 
 
-class HomeController:
+class HomeController():
     def index(self):
         response = render_template('pages/home/index.html.j2'), 200
         return response
