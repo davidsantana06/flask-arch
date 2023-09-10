21a6f79639cc360ff0e6cf016724d1fc9d9b4895
davidsantana06 <davidsantana06@hotmail.com>
2023-09-10 15:40:42 -0300

    feat: prepare environment for extensions, including the database

--- src/app/config/app.py
+++ src/app/config/app.py
@@ -1,5 +1,29 @@
 from flask import Flask
+from datetime import timedelta
+from typing import Dict
+
+
+APP_CONFIG_PARAMS: Dict[str, object] = {
+    'SECRET_KEY': '' + \
+        '| ------------------------------- |\n' + \
+        '|  Flask Definitive Architecture  |\n' + \
+        '|    github.com/davidsantana06    |\n' + \
+        '| ------------------------------- |',
+
+    'SQLALCHEMY_DATABASE_URI': '{dbms}://{username}:{password}@{server}/{database}'.format(
+        dbms='mysql+mysqlconnector',
+        username='',
+        password='',
+        server='',
+        database=''
+    ),
+
+    'SQLALCHEMY_TRACK_MODIFICATIONS': False,
+
+    'PERMANENT_SESSION_LIFETIME': timedelta(days=7)
+}
 
 
 def configure_app(app: Flask) -> None:
-    pass
+    for name, param in APP_CONFIG_PARAMS.items():
+        app.config[name] = param
--- src/app/config/extensions.py
+++ src/app/config/extensions.py
@@ -1,7 +1,10 @@
-from app.extensions import mvc
+from app.extensions import mvc, database, bcrypt, csrf
 
 from flask import Flask
 
 
 def configure_extensions(app: Flask) -> None:
     mvc.init_app(app)
+    # database.init_app(app)
+    bcrypt.init_app(app)
+    csrf.init_app(app)
