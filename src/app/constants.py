281002f7b11060d76b2039b0faddcead02475194
davidsantana06 <davidsantana06@hotmail.com>
2023-09-10 16:56:29 -0300

    refactor: move constants to the app directory

---
+++ src/app/constants.py
@@ -0,0 +1,11 @@
+from os import path
+
+
+SRC_FOLDER_PATH: str = path.abspath(
+    path.join(
+        path.dirname(__file__),
+        '..', '..'
+    )
+)
+OUTPUT_FOLDER_PATH: str = path.join(SRC_FOLDER_PATH, 'output')
+UPLOADS_FOLDER_PATH: str = path.join(SRC_FOLDER_PATH, 'uploads')
