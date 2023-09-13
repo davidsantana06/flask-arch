edd217e6ea3ad0c43529a18f11c5c0743cd8c594
davidsantana06 <davidsantana06@hotmail.com>
2023-09-13 00:12:41 -0300

    feat: add user repository

---
+++ src/app/core/base/repository.py
@@ -0,0 +1,31 @@
+from flask_sqlalchemy.model import Model
+from sqlalchemy import (
+    ColumnElement,
+    and_
+)
+from typing import List, Tuple
+from app.extensions import database
+
+
+class Repository():
+    def add_entity(self, model: Model) -> None:
+        database.session.add(model)
+        database.session.commit()
+
+    def delete_entity(self, model: Model) -> None:
+        database.session.delete(model)
+        database.session.commit()
+
+    def _find_by(self, model_cls: Model, filters: Tuple[ColumnElement[bool]]) -> Model:
+        model: Model = model_cls.query.filter(
+            and_(*filters)
+        ).first()
+
+        return model
+
+    def _find_all_by(self, model_cls: Model, filters: Tuple[ColumnElement[bool]]) -> List[Model]:
+        models: List[Model] = model_cls.query.filter(
+            and_(*filters)
+        ).all()
+
+        return models
---
+++ src/app/repositories/user_repository.py
@@ -0,0 +1,15 @@
+from typing import List
+from app.core.base.repository import Repository
+from app.models.user import User
+
+
+class UserRepository(Repository):
+    def find_by_username(self, username: str) -> User:
+        return self._find_by(
+            User, (User.username == username)
+        )
+
+    def find_all_by_name(self, name: str) -> List[User]:
+        return self._find_all_by(
+            User, (User.username == name)
+        )
