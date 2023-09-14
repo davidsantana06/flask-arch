a99cf32b970a9e8895d2fcc7b0cd894ab9b9ea79
davidsantana06 <davidsantana06@hotmail.com>
2023-09-13 21:23:35 -0300

    fix: change repository query filter from tuple to list

--- src/app/core/base/repository.py
+++ src/app/core/base/repository.py
@@ -3,7 +3,7 @@ from sqlalchemy import (
     ColumnElement,
     and_
 )
-from typing import List, Tuple
+from typing import List
 from app.extensions import database
 
 
@@ -16,14 +16,14 @@ class Repository():
         database.session.delete(model)
         database.session.commit()
 
-    def _find_by(self, model_cls: Model, filters: Tuple[ColumnElement[bool]]) -> Model:
+    def _find_by(self, model_cls: Model, filters: List[ColumnElement[bool]]) -> Model:
         model: Model = model_cls.query.filter(
             and_(*filters)
         ).first()
 
         return model
 
-    def _find_all_by(self, model_cls: Model, filters: Tuple[ColumnElement[bool]]) -> List[Model]:
+    def _find_all_by(self, model_cls: Model, filters: List[ColumnElement[bool]]) -> List[Model]:
         models: List[Model] = model_cls.query.filter(
             and_(*filters)
         ).all()
--- src/app/repositories/user_repository.py
+++ src/app/repositories/user_repository.py
@@ -6,10 +6,10 @@ from app.models.user import User
 class UserRepository(Repository):
     def find_by_username(self, username: str) -> User:
         return self._find_by(
-            User, (User.username == username)
+            User, [User.username == username]
         )
 
     def find_all_by_name(self, name: str) -> List[User]:
         return self._find_all_by(
-            User, (User.name == name)
+            User, [User.name.icontains(name)]
         )
