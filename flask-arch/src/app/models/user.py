696d9ef2bf4218765ca70d5c1989e7c2f7ba2bf1
davidsantana06 <davidsantana06@hotmail.com>
2023-09-16 19:10:05 -0300

    refactor: move repository responsibilities to model

--- flask-arch/src/app/models/user.py
+++ flask-arch/src/app/models/user.py
@@ -1,9 +1,11 @@
 from sqlalchemy import Column, DateTime, Integer, String
 from sqlalchemy.sql import func
+from typing import List
 from app.extensions import database
+from app.lib.base import BaseModel
 
 
-class User(database.Model):
+class User(BaseModel, database.Model):
     __tablename__: str = 'User'
 
     id: Column = Column(Integer, primary_key=True)
@@ -14,3 +16,11 @@ class User(database.Model):
     created_at: Column = Column(DateTime, nullable=False, default=func.now())
     updated_at: Column = Column(DateTime, nullable=False, default=func.now())
     status: Column = Column(Integer, nullable=False, default=1)
+
+    @classmethod
+    def find_by_username(cls, username: str) -> 'User':
+        return cls.find_by([User.username == username])
+
+    @classmethod
+    def find_all_by_name(cls, name: str) -> List['User']:
+        return cls.find_all_by([User.name.icontains(name)])
