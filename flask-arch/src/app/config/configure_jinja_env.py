41a0afec0adf41eb4f3a3f1786ca94c293e69208
davidsantana06 <davidsantana06@hotmail.com>
2023-09-20 14:29:56 -0300

    feat: add datetime formatter to jinja env

--- flask-arch/src/app/config/configure_jinja_env.py
+++ flask-arch/src/app/config/configure_jinja_env.py
@@ -1,9 +1,10 @@
 from flask import Flask
-from app.lib.utils import component, layout
+from app.lib.utils import component, format_datetime, layout
 
 
 def configure_jinja_env(app: Flask) -> None:
     app.jinja_env.globals.update({
         'component': component,
+        'format_datetime': format_datetime,
         'layout': layout
     })
