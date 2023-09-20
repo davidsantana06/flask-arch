92fc52591170c778cc0c93c7d1563f1b80e94379
davidsantana06 <davidsantana06@hotmail.com>
2023-09-19 22:29:27 -0300

    refactor: move CRUD methods to models file

--- flask-arch/src/app/models.py
+++ flask-arch/src/app/models.py
@@ -1,12 +1,59 @@
-from sqlalchemy import Column, DateTime, Integer, String
+from flask_sqlalchemy.model import Model
+from sqlalchemy import (
+    Column, ColumnElement, DateTime, Integer, String, UnaryExpression,
+    and_
+)
+from sqlalchemy.orm import Query
 from sqlalchemy.sql import func
 from typing import List
 
-from app.extensions import database
-from app.lib.base.models import BaseModel
+from .extensions import database
 
 
-class User(BaseModel, database.Model):
+class CRUDMixin(Model):
+    @staticmethod
+    def add(model: Model) -> None:
+        try:
+            database.session.add(model)
+            database.session.commit()
+        except Exception as e:
+            database.session.rollback()
+            raise e
+
+    @staticmethod
+    def delete(model: Model) -> None:
+        try:
+            database.session.delete(model)
+            database.session.commit()
+        except Exception as e:
+            database.session.rollback()
+            raise e
+
+    @classmethod
+    def find_by(cls, filters: List[ColumnElement[bool]]) -> Model:
+        model: Model = cls.query.filter(
+            and_(*filters)
+        ).first()
+
+        return model
+
+    @classmethod
+    def find_all(
+        cls, filter_clauses: List[ColumnElement[bool]] = None, order_clauses: List[UnaryExpression] = None
+    ) -> List[Model]:
+        query: Query = cls.query
+
+        if filter_clauses:
+            query = query.filter(and_(*filter_clauses))
+
+        if order_clauses:
+            query = query.order_by(*order_clauses)
+
+        models: List[Model] = query.all()
+        return models
+
+
+class User(database.Model, CRUDMixin):
     __tablename__: str = 'User'
 
     id: Column = Column(Integer, primary_key=True)
