756cadc75b8a666c1c1019534876620fd1cc5155
davidsantana06 <davidsantana06@hotmail.com>
2023-09-25 00:24:06 -0300

    refactor: change app env config from dict to class

--- flask-arch/src/app/config/config_app_env.py
+++ flask-arch/src/app/config/config_app_env.py
@@ -6,29 +6,26 @@ from app.constants import RESOURCES_FOLDER_PATH
 
 STATIC_FOLDER = f'{RESOURCES_FOLDER_PATH}/static'
 TEMPLATE_FOLDER = f'{RESOURCES_FOLDER_PATH}/templates'
-CONFIG = {
-    'SECRET_KEY': '' + \
+
+
+class ConfigAppEnv():
+    SECRET_KEY = '' + \
         '| ------------------------------- |\n' + \
-        '|  Flask Definitive Architecture  |\n' + \
+        '|           Flask Arch.           |\n' + \
         '|    github.com/davidsantana06    |\n' + \
-        '| ------------------------------- |',
-    'SQLALCHEMY_DATABASE_URI': '{dbms}://{username}:{password}@{server}/{database}'.format(
+        '| ------------------------------- |'
+    SQLALCHEMY_DATABASE_URI: '{dbms}://{username}:{password}@{server}/{database}'.format(
         dbms='mysql+mysqlconnector',
         username='',
         password='',
         server='',
         database=''
-    ),
-    'SQLALCHEMY_TRACK_MODIFICATIONS': False,
-    'PERMANENT_SESSION_LIFETIME': timedelta(days=1)
-}
+    )
+    SQLALCHEMY_TRACK_MODIFICATIONS: False
+    PERMANENT_SESSION_LIFETIME: timedelta(days=1)
 
 
 def configure_app_env(app: Flask) -> None:
     app.static_folder = STATIC_FOLDER
     app.template_folder = TEMPLATE_FOLDER
-    
-    for name, param in CONFIG.items():
-        app.config[name] = param
-
-        
+    app.config.from_object(ConfigAppEnv())
