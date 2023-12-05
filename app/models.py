c76839fdd966e216c52256df6c2d7f873aa9d120
davidsantana06 <davidsantana06@hotmail.com>
2023-12-05 16:15:07 -0300

    feat: add models division

--- app/models.py
+++ app/models.py
@@ -1,5 +1,7 @@
 from flask_login import UserMixin
 from flask_sqlalchemy.model import Model
+from flask_wtf import FlaskForm
+from re import findall
 from sqlalchemy import (
     Column, ColumnElement, DateTime, Integer, String, UnaryExpression,
     and_
@@ -11,11 +13,39 @@ from typing import List
 from .extensions import database
 
 
-class CRUDMixin(Model):
+# BASES/MIXINS #
+
+class Entity(Model):
     @declared_attr
     def __tablename__(cls):
-        return cls.__name__
+        cls_name_words = findall(r'[A-Z][a-z]*', cls.__name__)
+        table_name = '_'.join(cls_name_words).lower()
+        return table_name
 
+    @staticmethod
+    def save(entity: 'Entity') -> None:
+        try:
+            database.session.add(entity)
+            database.session.commit()
+        except Exception as e:
+            database.session.rollback()
+            raise e
+
+    @classmethod
+    def _find_first(cls, filter_clauses: List[ColumnElement[bool]]) -> 'Entity':
+        return cls.query.filter(and_(*filter_clauses)).first()
+
+    @classmethod
+    def _find_all(cls, filter_clauses: List[ColumnElement[bool]] = [], order_clauses: List[UnaryExpression] = None) -> List['Entity']:
+        query = cls.query.filter(and_(*filter_clauses))
+
+        if order_clauses:
+            query = query.order_by(*order_clauses)
+
+        return query.all()
+
+
+class MainEntity(Entity):
     @declared_attr
     def created_at(cls):
         return Column(DateTime, nullable=False, default=func.now())
@@ -29,46 +59,45 @@ class CRUDMixin(Model):
         return Column(Integer, nullable=False, default=1)
 
     @staticmethod
-    def add(model: Model) -> None:
+    def deactivate(entity: 'MainEntity') -> None:
         try:
-            database.session.add(model)
-            database.session.commit()
-        except Exception as e:
-            database.session.rollback()
-            raise e
-
-    @staticmethod
-    def delete(model: Model) -> None:
-        try:
-            database.session.delete(model)
+            entity.status = 0
+            database.session.add(entity)
             database.session.commit()
         except Exception as e:
             database.session.rollback()
             raise e
 
     @classmethod
-    def find_first(cls, filters: List[ColumnElement[bool]]) -> Model:
-        return cls.query.filter(
-            and_(*filters)
-        ).first()
+    def _find_first(cls, filter_clauses: List[ColumnElement[bool]]) -> 'MainEntity':
+        return cls._find_first([*filter_clauses, cls.status == 1])
 
     @classmethod
-    def find_all(
-        cls, filter_clauses: List[ColumnElement[bool]] = None, order_clauses: List[UnaryExpression] = None
-    ) -> List[Model]:
-        query = cls.query
-
-        if filter_clauses:
-            query = query.filter(and_(*filter_clauses))
-
-        if order_clauses:
-            query = query.order_by(*order_clauses)
-
-        return query.all()
+    def _find_all(cls, filter_clauses: List[ColumnElement[bool]] = [], order_clauses: List[UnaryExpression] = None) -> List['MainEntity']:
+        return cls._find_first([*filter_clauses, cls.status == 1], order_clauses)
 
 
-class User(database.Model, CRUDMixin, UserMixin):
-    id = Column(Integer, primary_key=True)
+class AssociationEntity(Entity):
+    ...
+
+
+class PopulateMixin:
+    def populate_form(self, form: FlaskForm) -> None:
+        for field in form:
+            if hasattr(self, field.name):
+                field.data = getattr(self, field.name)
+
+    def populate_obj(self, obj: object) -> None:
+        for attr in obj.__dict__:
+            if hasattr(self, attr):
+                value = getattr(self, attr)
+                setattr(obj, attr, value)
+
+
+# MODELS #
+
+class User(database.Model, MainEntity, PopulateMixin, UserMixin):
+    id = Column(Integer, autoincrement=True, unique=True, nullable=False, primary_key=True)
     name = Column(String(50), nullable=False)
     email = Column(String(50), unique=True, nullable=False)
     username = Column(String(30), unique=True, nullable=False)
@@ -76,12 +105,18 @@ class User(database.Model, CRUDMixin, UserMixin):
 
     @classmethod
     def find_by_id(cls, id: int) -> 'User':
-        return cls.find_first([User.id == id])
+        return cls._find_first([User.id == id])
 
     @classmethod
     def find_by_username(cls, username: str) -> 'User':
-        return cls.find_first([User.username == username])
+        return cls._find_first([User.username == username])
 
     @classmethod
     def find_all_by_name(cls, name: str) -> List['User']:
-        return cls.find_all([User.name.icontains(name)])
+        return cls._find_all([User.name.icontains(name)])
+
+    def __init__(self, name: str, email: str, username: str, password: str) -> None:
+        self.name = name
+        self.email = email
+        self.username = username
+        self.password = password
