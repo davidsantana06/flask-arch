c2984dce50b21f625c7ab224023d015bac98778f
davidsantana06 <davidsantana06@hotmail.com>
2023-12-11 14:33:35 -0300

    refactor: unify scripts into a single file

--- app/static/js/flash.js
+++
@@ -1,9 +0,0 @@
-const flashMessage = document.querySelector(".flash");
-
-if (flashMessage) {
-    document.addEventListener("DOMContentLoaded", () => { 
-        showMessageResponse(flashMessage.dataset.message, flashMessage.dataset.category);
-    });
-
-    flashMessage.remove();
-}
--- app/static/js/script.js
+++ app/static/js/script.js
@@ -60,3 +60,64 @@ function showMessageResponse(message, category, animationIn = "animate__bounceIn
     setTimeout(() => removeCurrentResponse(externalDiv, childDiv), 7 * 1000);
     document.querySelector("script").insertAdjacentElement("beforebegin", externalDiv);
 }
+
+
+function handleJsonResponse(data) {
+    if (data.redirect) {
+        window.location.replace(data.redirect);
+    } else {
+        showMessageResponse(data.message, data.category);
+    }
+}
+
+
+function jsonifyForm(form) {
+    form.addEventListener("submit", async (event) => {
+        event.preventDefault();
+
+        try {
+            const response = await fetch(form.dataset.action, {
+                method: form.dataset.method,
+                body: new FormData(form)
+            });
+            const data = await response.json();
+            handleJsonResponse(data);
+        } catch (error) {
+            console.error(error);
+        }
+    });
+}
+
+
+function jsonifyButton(button) {
+    button.addEventListener("click", async (event) => {
+        event.preventDefault();
+
+        try {
+            const response = await fetch(button.dataset.action, {
+                method: button.dataset.method,
+                headers: { "X-CSRFToken": button.dataset.csrf }
+            });
+            const data = await response.json();
+            handleJsonResponse(data);
+        } catch (error) {
+            console.error(error);
+        }
+    });
+}
+
+
+function handleFlashMessage() {
+    const flashMessage = document.querySelector(".flash");
+
+    if (flashMessage) {
+        document.addEventListener("DOMContentLoaded", () => { 
+            showMessageResponse(flashMessage.dataset.message, flashMessage.dataset.category);
+        });
+
+        flashMessage.remove();
+    }
+}
+
+
+handleFlashMessage();
